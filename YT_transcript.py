0ecc1f025a6a8c321641780d4119344f72273e49
nocode-shaurya <2021000507.shaurya@pg.sharda.ac.in>
2025-07-29 13:12:11 +0530

    basic functions working

--- YT_transcript.py
+++ YT_transcript.py
@@ -4,11 +4,8 @@
 from yt_transcript_fetcher import list_languages, get_transcript
 from googleapiclient.discovery import build
 import re
-from selenium import webdriver
-import time
 from selenium.webdriver.common.by import By
 import pandas as pd
-
 #=====================================================================================
 # Major Variables
 #=====================================================================================
@@ -18,90 +15,75 @@ keyword = "Today Breaking News"
 language = "hi"  # Language code for Hindi, 'en' for English, etc.
 target_video_count = 200  # Number of videos to fetch transcripts for
 trans_search_word = "भारत"  # Word to search for in the transcript
-
-
 #=====================================================================================
-# Get Video IDs
-#=====================================================================================
+youtube = build('youtube', 'v3', developerKey=api_key)
 
-driver = webdriver.Chrome()
-driver.get(channel_url)
-time.sleep(10)  # Wait for page to load (increase if needed)
-scroll_pause = 2
-for _ in range(target_video_count//10):  # Adjust for how many videos you want
-    driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
-    time.sleep(scroll_pause)
+channel_id = 'UCw0ry7cLRUnq3Oaszlhgqfg'  # Insert the channel ID you want to fetch videos from
 
-video_links = driver.find_elements(By.XPATH, '//a[@id="video-title-link"]')
+request = youtube.search().list(
+    part="id,snippet",
+    channelId=channel_id,
+    maxResults=50,     
+    order="date",
+    type="video"
+)
+trans_response = request.execute()
 
-# initiate blank list for dictionary
-video_data = []
-video_count = 0
-for link in video_links:
-    title = link.get_attribute('title')
-    href = link.get_attribute('href')
-    # print('Video Title:', title)
-    # print('Video Link:', href)
-    if re.search(keyword, title, re.IGNORECASE) != None and video_count < target_video_count:
-        print('Keyword found in title:', title)
-        if href and 'v=' in href:
-            video_id_full = href.split('v=')[1]
-            video_id = video_id_full.split('&')[0]  # Take only before '&'
+final_video_data = []
+final_video_ids = []
 
-            #get the transcript for the video
-            transcript_segments = get_transcript(video_id, language)
-            transcript = []
-            
-
-            if hasattr(transcript_segments, 'segments'):
-                for segment in transcript_segments.segments:
-                    transcript.append(segment.text)
-
-            # Count occurrences of the search word in the transcript
-            pattern = re.escape(trans_search_word)
-            trans_search_word_count = 0
-            for i in transcript:
-                trans_search_word_count += len(re.findall(pattern, i))
-            
-            # Fetch video details using YouTube Data API
-            youtube = build('youtube', 'v3', developerKey=api_key)
-            response = youtube.videos().list(
-                part="snippet, statistics, contentDetails",
-                id=video_id
-            ).execute()
-
-            video = response['items'][0]
-
-            title = video['snippet']['title']
-            published_at = video['snippet']['publishedAt']
-            duration = video['contentDetails']['duration']             # ISO 8601 format
-            view_count = video['statistics'].get('viewCount')
-            like_count = video['statistics'].get('likeCount')
-            comment_count = video['statistics'].get('commentCount')
-           
-            # append all video data as dicotionary to the list
-            video_data.append({
-                'video_id': video_id,
-                'title': title,
-                'published_at': published_at,
-                'duration': duration,
-                'view_count': view_count,
-                'like_count': like_count,
-                'comment_count': comment_count,
-                'keyword_search': f'{trans_search_word}',
-                'keyword_count': trans_search_word_count,
-            })
-            video_count += 1
+# Loop through the search results and filter by keyword in title
+for item in trans_response['items']:
+    title = item['snippet']['title']
+    video_id = item['id']['videoId']
+    #Check title for keyword
+    if re.search(keyword, title, re.IGNORECASE) != None:
+        final_video_ids.append(video_id)
     else:
         continue
 
+videos_data_response = youtube.videos().list(
+    part="snippet,statistics,contentDetails",
+    id=','.join(final_video_ids)
+).execute()
 
+for video in videos_data_response['items']:  
+    video_id = video['id']
+    title = video['snippet']['title']
+    published_at = video['snippet']['publishedAt']
+    duration = video['contentDetails']['duration']  # ISO 8601 format
+    view_count = video['statistics'].get('viewCount')
+    like_count = video['statistics'].get('likeCount')
+    comment_count = video['statistics'].get('commentCount')
 
-driver.quit()
+    #get transcript for the video
+    transcript_segments = get_transcript(video_id, language)
+    transcript = []
+    if hasattr(transcript_segments, 'segments'):
+        for segment in transcript_segments.segments:
+            transcript.append(segment.text)
+    # Count occurrences of the search word in the transcript
+    pattern = re.escape(trans_search_word)
+    trans_search_word_count = 0
+    for i in transcript:
+        trans_search_word_count += len(re.findall(pattern, i))
 
-#=====================================================================================
-# Save to CSV
-#=====================================================================================
-df = pd.DataFrame(video_data)
-df.to_csv('video_data_testing.csv', index=False, encoding='utf-8-sig')
-print(df)
+    # Append all video data as dictionary to the list
+    final_video_data.append({
+        'video_id': video_id,
+        'title': title,
+        'published_at': published_at,
+        'duration': duration,
+        'view_count': view_count,
+        'like_count': like_count,
+        'comment_count': comment_count,
+        'search_word' : f'{trans_search_word}',
+        'trans_search_word_count': trans_search_word_count
+    })
+
+print(f'Total videos found: {len(final_video_data)}')
+print()
+for video in final_video_data:
+    print(video)
+    print()
+
